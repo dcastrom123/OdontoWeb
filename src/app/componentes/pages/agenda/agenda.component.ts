fb67abb30db4185c5e45432badeb72bcfd895214
dcastrom123 <diego.castro.maira+gmail.com@gmail.com>
2023-12-19 01:11:57 -0300

    vista especialidad por profesional

--- src/app/componentes/pages/agenda/agenda.component.ts
+++ src/app/componentes/pages/agenda/agenda.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient  } from '@angular/common/http';
+import { ChangeDetectorRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-agenda',
@@ -19,15 +21,15 @@ especialidades: any[] = [];
 // Variables para almacenar el profesional y especialidad seleccionado
 selectedProfesional: any;
 selectedEspecialidad: any;
+selectedProfesionalId: string | undefined;
 
-
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
        // Llamada a la API para obtener la lista de profesionales al iniciar el componente
-       this.obtenerProfesionales();
-       this.obtenerEspecialidades();
+      this.obtenerProfesionales();
+      this.obtenerEspecialidades();
+      this.obtenerEspprofesional();
   }
 //Profesionales
   obtenerProfesionales() {
@@ -40,8 +42,50 @@ selectedEspecialidad: any;
       }
     );
   }
+
+  //Seleccionar profesional y guardarlo para usar en el siguiente paso
   seleccionarProfesional(profesional: any) {
     this.selectedProfesional = profesional;
+    // Llamada a la API para obtener la lista de especialidades del profesional seleccionado
+    this.obtenerEspprofesional();
+  }
+//seleccionar desde dropdowns
+  seleccionarProfesionalDesdeDropdown(event: any) {
+    let selectedProfesionalId = event.target.value;
+    this.selectedProfesional = this.profesionales.find(profesional => profesional.id === this.selectedProfesionalId);
+    
+    console.log(selectedProfesionalId);
+    this.cdr.detectChanges();
+
+  }
+
+
+  //Obtener especialidades por profesional seleccionado
+  obtenerEspprofesional(){
+    /*const _id = this.selectedProfesional._id;
+    if (_id) {
+      this.http.get<any[]>(`http://localhost:3700/api/auth/profesionales/${_id}/especialidades`).subscribe(
+        (data) => {
+          this.especialidades = data;
+        },
+        (error) => {
+          console.error('Error al obtener la lista de especialidades:', error);
+        }
+      );
+    }
+    console.log("seleccionado dr muestra especialidad");
+  */
+    if (this.selectedProfesionalId) {
+      this.http.get<any[]>(`http://localhost:3700/api/auth/profesionales/${this.selectedProfesionalId}/especialidades`).subscribe(
+        (data) => {
+          this.especialidades = data;
+        },
+        (error) => {
+          console.error('Error al obtener la lista de especialidades:', error);
+        }
+      );
+      console.log("seleccionado dr muestra especialidad");
+    }
   }
 
   //Especialidades
@@ -55,6 +99,7 @@ selectedEspecialidad: any;
       }
     );
   }
+
 
 
 
